53b991a74df1f6c35d67b0a88607a0fef018f452
Baertram <pz1@gmx.net>
2022-04-06 00:17:49 +0200

    -Fixed bug #220

--- FCOItemSaver/FCOItemSaver.lua
+++ FCOItemSaver/FCOItemSaver.lua
@@ -155,19 +155,6 @@ user:/AddOns/FCOItemSaver/src/FCOIS_Events.lua:1128: in function 'FCOItemSaver_L
 
 --#176, 2021-10-12, Beartram: Errors occured with OR filters, and mixed AND + OR filters -> Currently only ALL or or ALL AND can be enabled!
 --#181, 2022-01-02, Baertram: Check filter slash command chat feedback: Does it show correct info about filter state and new logical conjuncions?
---#220, 2022-04-02, Caramoon, FCOIS comments: Using the crafting tables deconstruction to filter items with 2x green filters (lock, sell) and 1x yellow (decon) wills till show items
---which got the sell icon active (green> should be hidden) and the decon icon active (yellow > should be shown). Logical conjunction mode: ?? unknown, supposed: AND
---[[
-So I recently notice an issue that popped up. When I'm at a crafting table, specifically in the deconstruction mode, when the filter for deconstruction flagged items is yellow (show only) the filter for selling/sell in guild/intricate doesn't have any effect. The happens at all four crafting station types.
-
-The add-ons settings page says I'm running version 224, and everything should be up to date since I use Minion.
-
-Here's a screenshot, and I can provide a video if I'm not describing it well enough.
-
-https://imgur.com/a/hRtnAo7
-
-(the two items are flagged both "decon" and "sell at guild trader")
-]]
 
 
 --______________________________________
@@ -176,20 +163,20 @@ https://imgur.com/a/hRtnAo7
 
 
 ------------------------------------------------------------------------------------
--- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-04-03
+-- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-04-05
 ------------------------------------------------------------------------------------
---#220 Filter at crafting table deconstruction shows items marked for deconstruction AND sell in guildstore even though only decon marker
--- icon filter is yellow (show only) but sell at guildstore amrker icon is green (hide), and the logical conjunction of the filters is set
--- AND -> Should check for yellow (only show) AND green (hide) = hide. But works like a logical OR conjunction here.
 
 
 -------------------------------------------------------------------------------------
---Changelog (last version: 2.2.4 - New version: 2.2.5) -    Updated last: 2022-04-03
+--Changelog (last version: 2.2.4 - New version: 2.2.5) -    Updated last: 2022-04-05
 -------------------------------------------------------------------------------------
 --[Fixed]
 --#217 Error at mouse hover over inventory quest items
 --#218 Error at LAM settings menu as LAM icon dropdowns are created
 --#219 Non set collection items were tried to be bound and chat output told you they were bound
+--#220 Filter at crafting table deconstruction shows items marked for deconstruction AND sell in guildstore even though only decon marker
+-- icon filter is yellow (show only) but sell at guildstore marker icon is green (hide), and the logical conjunction of the filters is set
+-- AND -> Should check for yellow (only show) AND green (hide) = hide. But works like a logical OR conjunction here.
 
 
 --[Changed]
