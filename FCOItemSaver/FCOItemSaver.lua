8514de5de8eccfecd5053f0104881d315756fc00
Baertram <pz1@gmx.net>
2022-08-18 19:35:08 +0200

    --#243 2022-08-18, Baertram, Feature request: Change LibShifterBox usage to LibAddonMenuDualListBox widget in settings etc.!
    --______________________________________
    -- Current max # of bugs/features/ToDos: 243
    --______________________________________
    --#243
    
    ------------------------------------------------------------------------------------
    -- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-08-18
    ------------------------------------------------------------------------------------
    --#243
    
    -------------------------------------------------------------------------------------
    --Changelog (last version: 2.3.2 - New version: 2.3.3) -    Updated last: 2022-08-18
    -------------------------------------------------------------------------------------
    --[Fixed]
    
    --[Changed]
    --Changed the folder structure of the addon. Do not keep old folder live/AddOns/FCOItemSaver! Do not extarct the zip file "over" the existing folder. Delete the folder and install the update so a new live/AddOns/FCOItemSaver folder will be created
    
    --[Added]
    
    --[Added on request]

--- FCOItemSaver/FCOItemSaver.lua
+++ FCOItemSaver/FCOItemSaver.lua
@@ -176,53 +176,33 @@ user:/AddOns/FCOItemSaver/src/FCOIS_Events.lua:1128: in function 'FCOItemSaver_L
 --Sorry if this is explained somewhere, but is it possible to have the New items Automatic marking exclude item categories?
 --A lot of junk items and white weapons/armor filll my inventory, I want them to be marked as sold, but I don't want it to apply to things like materials/consumables. Thank you
 
---#238 2022-07-17, Baertram, Feature idea: Speed-up the AddMark function and cache some markId independent checks so that calls to he same function AddMark with the same bagId and slotIndex
--- can reuse the cached results. change of bagId or change of slotIndex will reset the cache.
-
 --#239 2022-07-17, Baertram, bug: AwesomeGuildStore - Directly switching from custom "Sell from bank" button to "Sell from inventory" button will enable FCOIS.gFilterWhere = LF_INVENTORY
 --instead of LF_GUILD_STORE_SELL
 -->Maybe event_bank_closed?
 
---#242 2022-08-14, Baertram, Feature idea: Add the 4 filter buttons to LF_SMITHING_RESEARCH / LF_JEWELRY_RESEARCH to filter the shown items already at the list,
---  before selecting an item and showing the LF_SMITHING_RESEARCH_DIALOG popup
-
+--#243 2022-08-18, Baertram, Feature request: Change LibShifterBox usage to LibAddonMenuDualListBox widget in settings etc.!
 
 --______________________________________
--- Current max # of bugs/features/ToDos: 242
+-- Current max # of bugs/features/ToDos: 243
 --______________________________________
-
-
---Todo for this patch
+--#243
 
 ------------------------------------------------------------------------------------
--- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-08-17
+-- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-08-18
 ------------------------------------------------------------------------------------
-
+--#243
 
 -------------------------------------------------------------------------------------
---Changelog (last version: 2.3.1 - New version: 2.3.2) -    Updated last: 2022-08-17
+--Changelog (last version: 2.3.2 - New version: 2.3.3) -    Updated last: 2022-08-18
 -------------------------------------------------------------------------------------
 --[Fixed]
---
+
 
 --[Changed]
---API function FCOIS.GetIconText provides more parameters now:
---Global function to get the for a given gear set's iconId (2, 4, 6, 7 or 8) or a dynamic icon id (13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
---> use the constants for the marker icons please! e.g. FCOIS_CON_ICON_LOCK, FCOIS_CON_ICON_DYNAMIC_1 etc. Check file src/FCOIS_constants.lua for the available constants (top of the file)
---boolean withTexture <optional>: Add the icon#s texture to the name (default: left side)
---boolean textureAtRight <optional>: Put the texture at the right side of the name
---boolean textureNonColored <optional>: If true the texture will not be colored explicitly, if false the texture will use the color of the icon settings
---function FCOIS.GetIconText(iconId, withTexture, textureAtRight, textureNonColored)
 
 
 --[Added]
---#238 Speed-up the AddMark function and cache some markId independent checks so that calls to the same function AddMark with the same bagId and slotIndex can reuse the cached results
---#241 Added setting to add a "remove all"/"restore last marker icons" to the context menu of items. You need to enable this at the settings submenu "marker icons" -> "Undo".
---Undo entries saved by SHIFT+right mouse click (if enabled at the settings) or via this new context menu entry will be cleared if you manually set a new marker icon on the same item
---via the inventory context menu!
---Keybinds or mass-marking will not overwrite them.
---Added tooltip setting for that new setting (see above) to show the last marked marker icons at the item if you press and hold the SHIFT key
---#242 Add the 4 filter buttons to LF_SMITHING_RESEARCH / LF_JEWELRY_RESEARCH to filter the shown items already at the list before the item selection popup shows
+
 
 --[Added on request]
 
--- README.md
+++ README.md
@@ -1,40 +1,28 @@
 ```
---______________________________________
--- Current max # of bugs/features/ToDos: 242
---______________________________________
+--#243 2022-08-18, Baertram, Feature request: Change LibShifterBox usage to LibAddonMenuDualListBox widget in settings etc.!
 
-
---Todo for this patch
+--______________________________________
+-- Current max # of bugs/features/ToDos: 243
+--______________________________________
+--#243
 
 ------------------------------------------------------------------------------------
--- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-08-17
+-- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-08-18
 ------------------------------------------------------------------------------------
-
+--#243
 
 -------------------------------------------------------------------------------------
---Changelog (last version: 2.3.1 - New version: 2.3.2) -    Updated last: 2022-08-17
+--Changelog (last version: 2.3.2 - New version: 2.3.3) -    Updated last: 2022-08-18
 -------------------------------------------------------------------------------------
 --[Fixed]
---
+
 
 --[Changed]
---API function FCOIS.GetIconText provides more parameters now:
---Global function to get the for a given gear set's iconId (2, 4, 6, 7 or 8) or a dynamic icon id (13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
---> use the constants for the marker icons please! e.g. FCOIS_CON_ICON_LOCK, FCOIS_CON_ICON_DYNAMIC_1 etc. Check file src/FCOIS_constants.lua for the available constants (top of the file)
---boolean withTexture <optional>: Add the icon#s texture to the name (default: left side)
---boolean textureAtRight <optional>: Put the texture at the right side of the name
---boolean textureNonColored <optional>: If true the texture will not be colored explicitly, if false the texture will use the color of the icon settings
---function FCOIS.GetIconText(iconId, withTexture, textureAtRight, textureNonColored)
-
+--Changed the addon's folder structure (make sure to completely delete the live/AddOns/FCOItemSaver folder before installing this update!
+-->Do not keep any older files and folders, do not simly extract the zip archive "over" the existing folder!!!
 
 --[Added]
---#238 Speed-up the AddMark function and cache some markId independent checks so that calls to the same function AddMark with the same bagId and slotIndex can reuse the cached results
---#241 Added setting to add a "remove all"/"restore last marker icons" to the context menu of items. You need to enable this at the settings submenu "marker icons" -> "Undo".
---Undo entries saved by SHIFT+right mouse click (if enabled at the settings) or via this new context menu entry will be cleared if you manually set a new marker icon on the same item
---via the inventory context menu!
---Keybinds or mass-marking will not overwrite them.
---Added tooltip setting for that new setting (see above) to show the last marked marker icons at the item if you press and hold the SHIFT key
---#242 Add the 4 filter buttons to LF_SMITHING_RESEARCH / LF_JEWELRY_RESEARCH to filter the shown items already at the list before the item selection popup shows
+
 
 --[Added on request]
 ```
