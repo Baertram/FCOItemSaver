a66bb6d0bafb5dd44677df6e34605c9ffddac4e3
Baertram <pz1@gmx.net>
2022-10-10 21:42:02 +0200

    --______________________________________ -- Current max # of bugs/features/ToDos: 253 --______________________________________
    ------------------------------------------------------------------------------------
    -- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-10-10
    ------------------------------------------------------------------------------------
    
    -------------------------------------------------------------------------------------
    --Changelog (last version: 2.3.8 - New version: 2.3.9) -    Updated last: 2022-10-10
    -------------------------------------------------------------------------------------
    --[Fixed]
    --#251 IIfA right click on mementos shwos error message: FCOIS_ContextMenus.lua:1071: Operator .. is not supported for nil .. string
    --#252 Keybind of "Sell at guild store" (and other special marker icons depending on your settings) does not check if item can be sold at a guild store (non bound e.g.)
    --#253 Used proper FCOIS_CON* constants instead of numbers at the keybinds
    
    --[Changed]
    --
    
    --[Added]
    --
    
    --[Added on request]
    --

--- FCOItemSaver/FCOItemSaver.lua
+++ FCOItemSaver/FCOItemSaver.lua
@@ -182,8 +182,6 @@ user:/AddOns/FCOItemSaver/src/FCOIS_Events.lua:1128: in function 'FCOItemSaver_L
 
 --#248 2022-08-18, Baertram, Feature request: Change LibShifterBox usage to LibAddonMenuDualListBox widget in settings etc.!
 
---#252 2022-10-07, Baertram, bug: Keybind of "Sell at guild store" does not check if item can be sold at a guild store (non bound e.g.)
-
 
 
 --______________________________________
@@ -194,13 +192,14 @@ user:/AddOns/FCOItemSaver/src/FCOIS_Events.lua:1128: in function 'FCOItemSaver_L
 ------------------------------------------------------------------------------------
 -- Currently worked on [Added/Fixed/Changed] -              Updated last 2022-10-10
 ------------------------------------------------------------------------------------
---#252
+
 
 -------------------------------------------------------------------------------------
 --Changelog (last version: 2.3.8 - New version: 2.3.9) -    Updated last: 2022-10-10
 -------------------------------------------------------------------------------------
 --[Fixed]
 --#251 IIfA right click on mementos shwos error message: FCOIS_ContextMenus.lua:1071: Operator .. is not supported for nil .. string
+--#252 Keybind of "Sell at guild store" (and other special marker icons depending on your settings) does not check if item can be sold at a guild store (non bound e.g.)
 --#253 Used proper FCOIS_CON* constants instead of numbers at the keybinds
 
 
